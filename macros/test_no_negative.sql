be6ad73386f2764afc21100dd6d162c28b1cbf3d
Paul García <paulm.gar@gmail.com>
2025-09-26 23:12:29 +0000

    update source to snowflake, add no negative tests and clean repo

--- macros/test_no_negative.sql
+++ macros/test_no_negative.sql
@@ -1,6 +1,5 @@
 {# macros/test_no_negative.sql #}
+
 {% macro test_no_negative(model, column_name) %}
-select *
-from {{ model }}
-where {{ column_name }} < 0
+    select * from {{ model }} where {{ column_name }} < 0
 {% endmacro %}
